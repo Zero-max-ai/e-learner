5fd63c9e77d90829c66841aa25b5f0efc6e72adf
Zero-max-ai <75124097+Zero-max-ai@users.noreply.github.com>
2022-06-30 21:54:51 +0530

    Create course.js

---
+++ course/course.js
@@ -0,0 +1,26 @@
+// loader
+var loader = document.getElementById("preloader");
+window.addEventListener("load",function(){
+  loader.style.display = "none"
+})
+//end loader
+
+// hamburger
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navlinks");
+const navLink = document.querySelectorAll(".navlink");
+
+hamburger.addEventListener("click",mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+// end hamburger
